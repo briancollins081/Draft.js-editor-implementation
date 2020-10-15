de5b39deae74009781b64ca5b9bc2d72aedec8ec
Brian Andere <briancollins081@gmail.com>
2020-10-15 11:23:13 +0300

    Initialized editor + added Rich Utils for text editing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import 'draft-js/dist/Draft.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    editorState: EditorState.createEmpty()
+  }
+  handleOnEditorChange = editorState => {
+    this.setState({
+      editorState
+    });
+  }
+
+  handelKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.handleOnEditorChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  render() {
+    return (
+      <div className="medium-container content-section">
+        <header className="vertical-center">
+          <h1>Draft.js Editor</h1>
+        </header>
+        <hr />
+        <div className="editor-section">
+          <h4 className="text-center">Editing</h4>
+          <div className="editor-wrapper">
+            <Editor
+              editorState={this.state.editorState}
+              onChange={this.handleOnEditorChange}
+              handleKeyCommand={this.handelKeyCommand} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
