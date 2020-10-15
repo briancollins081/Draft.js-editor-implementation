2a5e90e0a9afd028ffa3f3a128bdb645b3ac9c2b
Brian Andere <briancollins081@gmail.com>
2020-10-15 12:31:26 +0300

    Added linkify plugin - detects links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js';
+
+import { EditorState, RichUtils } from 'draft-js';
+import Editor  from 'draft-js-plugins-editor';
+import createLinkifyPlugin from 'draft-js-linkify-plugin';
+
 import 'draft-js/dist/Draft.css';
+import 'draft-js-linkify-plugin/lib/plugin.css';
+
+
+
 import './App.css';
+
+// Draft constants
+const linkifyPlugin = createLinkifyPlugin({
+  component: (props) => (
+    // eslint-disable-next-line no-alert, jsx-a11y/anchor-has-content
+    <a {...props} onClick={() => alert('Clicked on Link!')} />
+  )
+});
+const plugins = [linkifyPlugin]
 
 class App extends React.Component {
   state = {
@@ -21,6 +38,9 @@ class App extends React.Component {
     }
     return 'not-handled';
   }
+  handleEditorFocus = () => {
+    this.editor.focus();
+  };
 
   render() {
     return (
@@ -31,11 +51,14 @@ class App extends React.Component {
         <hr />
         <div className="editor-section">
           <h4 className="text-center">Editing</h4>
-          <div className="editor-wrapper">
+          <div className="editor-wrapper" onClick={this.handleEditorFocus}>
             <Editor
               editorState={this.state.editorState}
               onChange={this.handleOnEditorChange}
-              handleKeyCommand={this.handelKeyCommand} />
+              handleKeyCommand={this.handelKeyCommand}
+              plugins={plugins}
+              ref={(ref) => { this.editor = ref; }}
+            />
           </div>
         </div>
       </div>
